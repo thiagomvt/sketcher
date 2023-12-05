e848eb09aa469b5baadb97cb5953ef490f85c2aa
Thiago <143139077+thiagomvt@users.noreply.github.com>
2023-12-05 18:57:34 -0300

    Add functionalitites
    Create a button to change the total number of panels,
    newSketcher function now resizes the created panel to fit the container,
    make small CSS changes.

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 // Create a grid of 16x16 divs
 
 let container = document.querySelector('#container');
+let squareSize = 16;
+let button = document.querySelector('#button');
 
-for (i=1; i<=16*16; i++){
-      let panel = document.createElement('div');
-      container.appendChild(panel);
-      panel.classList.add('panel');
+newSketcher(squareSize);
+
+button.addEventListener('click', (e) => {
+squareSize = prompt('What\'s the desired square size? (Max: 100)')
+newSketcher(squareSize)
+
+});
+
+
+
+
+function newSketcher(squareSize){
+
+      let allPanels = document.querySelectorAll('.panel');
+      allPanels.forEach(element => {
+      container.removeChild(element)
+      });
+
+      let panelSize = (800/squareSize);
+
+      for (i=1; i<=squareSize*squareSize; i++){
+            let newPanel = document.createElement('div');
+            container.appendChild(newPanel);
+            newPanel.classList.add('panel');
+            newPanel.style.cssText = (`width: ${800/squareSize}px; height: ${800/squareSize}px`);
+      }
 }
+
